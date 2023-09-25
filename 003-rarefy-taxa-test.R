fa81dba1ea17cdd996adc66359eac8054461b861
LiYingWang <jaybin502@gmail.com>
2023-09-25 09:11:20 -0700

    write functions

--- 003-rarefy-taxa-test.R
+++ 003-rarefy-taxa-test.R
@@ -4,14 +4,22 @@ library(tidyverse)
 library(broom)
 library(Hmisc)
 library(readxl)
-shared_V4_rarefy <-
-  read.csv(here::here("analysis","data","derived_data","V4_shared_format.1.subsample.60216.csv"),
-           sep = ",") %>%
-  select(Group, starts_with("ASV")) %>%
-  pivot_longer(-Group, names_to="otu", values_to="count")
 
-taxonomy_V4 <-
-  read.table(here::here("analysis","data","raw_data","V4_asv_table.txt"), header = T, sep="\t") %>%
+# function to read data
+shared_rarefy_long <- function(filename, vec){
+  read.csv(here::here("analysis","data","derived_data", filename), sep = vec) %>%
+    select(Group, starts_with("ASV")) %>%
+    pivot_longer(-Group, names_to="otu", values_to="count")
+}
+
+shared_V4_rarefy <-shared_rarefy_long("V4_shared_format.1.subsample.60216.csv", ",")
+shared_V1_rarefy <-shared_rarefy_long("V1_shared_format.1.subsample.13658.csv", "")
+shared_V6_rarefy <-shared_rarefy_long("V6_shared_format.1.subsample.28402.csv", ",")
+shared_ITS1_rarefy <-shared_rarefy_long("ITS1_shared_format.1.subsample.387.csv", ",")
+
+# function to tidy taxonomy data
+tidy_taxonomy <- function(filename){
+  read.table(here::here("analysis","data","raw_data",filename), header = T, sep="\t") %>%
   select(OTU.ID, taxonomy) %>%
   rename(otu = OTU.ID) %>%
   mutate(taxonomy = str_replace_all(taxonomy, "\\(\\d+\\)", ""),
@@ -22,48 +30,90 @@ taxonomy_V4 <-
          taxonomy = str_replace(taxonomy, "(; s__)([^(; s__)]*)$", ""),
          taxonomy = str_replace(taxonomy, "(; f__)([^(; f__)]*)$", ""),
          taxonomy = str_replace(taxonomy, "(; g__)([^(; g__)]*)$", ""))
+  }
 
-metadata_V4 <-
-  read_excel(here::here("analysis","data", "raw_data", "GJB_amp_meta.xlsx")) %>%
-  filter(Region == "V4") %>%
-  rename(Group = `adpater no`) %>%
-  mutate(type= str_extract(Group, "[[:alpha:]]+(?=\\d)"), type = tolower(type)) %>%
-  mutate(group = if_else(!is.na(area)&!area%in% c("belly", "outside belly"),
-                         paste0(type, "-", area), type),
-         pot = case_when(group == "pot-inside" ~ "interior", group == "pot-outside" ~"exterior"),
-         potex_soil = case_when(group == "pot-outside" ~ "pot-exterior", type== "soil" ~ "soil"),
-         potin_soil = case_when(group == "pot-inside" ~ "pot-interior", type== "soil" ~ "soil"),
-         pot_con = case_when(type== "pot" ~ "pot", type== "con" ~ "control"),
-         potex_con = case_when(group == "pot-outside" ~ "pot-exterior", type== "con" ~ "control"),
-         potin_con = case_when(group == "pot-inside" ~ "pot-interior", type== "con" ~ "control")) %>%
-  relocate(Group, type, group, sample, area, site)
+taxonomy_V4 <- tidy_taxonomy("V4_asv_table.txt")
+taxonomy_V1 <- tidy_taxonomy("V1_asv_table.txt")
+taxonomy_V6 <- tidy_taxonomy("V6_asv_table.txt")
+taxonomy_ITS1 <- tidy_taxonomy("ITS1_asv_table.txt")
 
-# taxa level not ASV level
-composite_V4 <-
-  inner_join(shared_V4_rarefy, taxonomy_V4, by= "otu") %>%
-  group_by(Group, taxonomy) %>%
-  dplyr::summarize(count = sum(count), .groups="drop") %>%
-  group_by(Group) %>%
-  mutate(rel_abund = count / sum(count)) %>%
-  ungroup() %>%
-  select(-count) %>%
-  inner_join(., metadata_V4, by = "Group")
+# function to read metadata for each region
+meta_region_extract <- function(filename, vec){
+  read_excel(here::here("analysis","data", "raw_data", filename)) %>%
+    filter(Region == vec) %>%
+    rename(Group = `adpater no`) %>%
+    mutate(type= str_extract(Group, "[[:alpha:]]+(?=\\d)"), type = tolower(type)) %>%
+    mutate(group = if_else(!is.na(area)&!area%in% c("belly", "outside belly"),
+                           paste0(type, "-", area), type),
+           pot = case_when(group == "pot-inside" ~ "interior", group == "pot-outside" ~"exterior"),
+           potex_soil = case_when(group == "pot-outside" ~ "pot-exterior", type== "soil" ~ "soil"),
+           potin_soil = case_when(group == "pot-inside" ~ "pot-interior", type== "soil" ~ "soil"),
+           pot_con = case_when(type== "pot" ~ "pot", type== "con" ~ "control"),
+           potex_con = case_when(group == "pot-outside" ~ "pot-exterior", type== "con" ~ "control"),
+           potin_con = case_when(group == "pot-inside" ~ "pot-interior", type== "con" ~ "control")) %>%
+    relocate(Group, type, group, sample, area, site)
+}
 
-sig_genera <-
+metadata_V4 <- meta_region_extract("GJB_amp_meta.xlsx", "V4")
+metadata_V1 <- meta_region_extract("GJB_amp_meta.xlsx", "V1")
+metadata_V6 <- meta_region_extract("GJB_amp_meta.xlsx", "V6")
+metadata_ITS1 <- meta_region_extract("GJB_amp_meta.xlsx", "ITS1")
+
+# function to calculate relative abundance for taxa level, not ASV level
+composite_region <- function(df1, df2, df3){
+  inner_join(df1, df2, by= "otu") %>%
+    group_by(Group, taxonomy) %>%
+    dplyr::summarize(count = sum(count), .groups="drop") %>%
+    group_by(Group) %>%
+    mutate(rel_abund = count / sum(count)) %>%
+    ungroup() %>%
+    select(-count) %>%
+    inner_join(., df3, by = "Group") %>%
+    filter(!taxonomy == "k__Bacteria")
+}
+
+composite_V4 <- composite_region(shared_V4_rarefy, taxonomy_V4, metadata_V4)
+composite_V1 <- composite_region(shared_V1_rarefy, taxonomy_V1, metadata_V1)
+composite_V6 <- composite_region(shared_V6_rarefy, taxonomy_V6, metadata_V6)
+composite_ITS1 <- composite_region(shared_ITS1_rarefy, taxonomy_ITS1, metadata_ITS1)
+
+# get significant genera
+sig_genera_V4 <-
   composite_V4 %>%
   nest(data = -taxonomy) %>% #c(-taxonomy, -otu)
-  mutate(test = map(.x=data, ~wilcox.test(rel_abund~  pot, data=.x, exact = FALSE) %>% tidy)) %>% #run broom first
+  mutate(test = map(.x= data, ~wilcox.test(rel_abund~  pot_con, data=.x, exact = FALSE) %>% tidy)) %>% #run broom first
   unnest(test) %>%
-  filter(p.value < 0.05)
+  filter(p.value < 0.1)
   #mutate(p.adjust = p.adjust(p.value, method="BH")) %>% #https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6099145/
   #filter(p.adjust < 0.01) %>%
   #select(taxonomy, p.adjust)
+
+sig_genera_V1 <-
+  composite_V1 %>%
+  nest(data = -taxonomy) %>% #c(-taxonomy, -otu)
+  mutate(test = map(.x= data, ~wilcox.test(rel_abund~  pot_con, data=.x, exact = FALSE) %>% tidy)) %>% #run broom first
+  unnest(test) %>%
+  filter(p.value < 0.1)
+
+sig_genera_V6 <-
+  composite_V6 %>%
+  nest(data = -taxonomy) %>% #c(-taxonomy, -otu)
+  mutate(test = map(.x= data, ~wilcox.test(rel_abund~  pot_con, data=.x, exact = FALSE) %>% tidy)) %>% #run broom first
+  unnest(test) %>%
+  filter(p.value < 0.1)
+
+sig_genera_ITS1 <-
+  composite_ITS1 %>%
+  nest(data = -taxonomy) %>% #c(-taxonomy, -otu)
+  mutate(test = map(.x= data, ~wilcox.test(rel_abund~  pot_con, data=.x, exact = FALSE) %>% tidy)) %>% #run broom first
+  unnest(test) %>%
+  filter(p.value < 0.1)
 
 # make a plot
 library(ggtext)
 composite_V4_plot <-
   composite_V4 %>%
-  inner_join(sig_genera, by="taxonomy") %>%
+  inner_join(sig_genera_V4, by="taxonomy") %>%
   filter(!is.na(pot)) %>%
   mutate(taxa = ifelse(str_detect(taxonomy, "g__"), str_extract(taxonomy, "(g__)([A-z]+)", group =2),
                        str_extract(taxonomy, "(f__)([A-z]+)", group =2)), #"g__[A-z]+
